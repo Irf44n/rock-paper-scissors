8d4d3a8830e02b48d1e929720b41f98693bbbf71
Irfaan <146428129+Irf44n@users.noreply.github.com>
2024-06-18 19:26:58 +0100

    Create getComputerChoice function
    created the function that returns the computer choice either rock, paper or scissors

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -1,1 +1,12 @@
-console.log("Hello")
+function getComputerChoice() {
+    let number = Math.random()*30;
+    if (number < 10) {
+        return "rock"
+    } else if (number < 20) {
+        return "paper"
+    } else {
+        return "scissors"
+    };
+}
+
+console.log(getComputerChoice());
