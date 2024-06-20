710cc0d85b1dcdc44767d814567f8b72816471d8
Irfaan <146428129+Irf44n@users.noreply.github.com>
2024-06-20 11:59:31 +0100

    Create playGame function

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -30,27 +30,49 @@ function getHumanChoice() {
 }
 
 
-function playRound(humanChoice, computerChoice) {
+function playGame() {
+    let humanScore = 0, computerScore = 0;
 
-    if (humanChoice === null) {
-        return 0
+    function playRound(humanChoice, computerChoice) {
+
+        if (humanChoice === null) {
+            return ''
+        };
+    
+    
+        if (humanChoice == computerChoice) {
+            return "It's a tie."
+        } else if (humanChoice == "rock" && computerChoice == "scissors" || humanChoice == "paper" && computerChoice == "rock" || humanChoice == "scissors" && computerChoice == "paper") {
+            humanScore += 1;
+            return (`You WON!!! ${humanChoice} beats ${computerChoice}`)
+        } else {
+            computerScore += 1;
+            return (`You LOST!!! ${computerChoice} beats ${humanChoice}`)
+        };
     }
 
 
-    if (humanChoice == computerChoice) {
-        return "It's a tie."
-    } else if (humanChoice == "rock" && computerChoice == "scissors" || humanChoice == "paper" && computerChoice == "rock" || humanChoice == "scissors" && computerChoice == "paper") {
-        humanScore += 1;
-        return (`You WON!!! ${humanChoice} beats ${computerChoice}`)
-    } else {
-        computerScore += 1
-        return (`You LOST!!! ${computerChoice} beats ${humanChoice}`)
+    function showScore() {
+        return `YOU: ${humanScore} COMPUTER: ${computerScore}`;
     }
+
+
+    for (i=0; i<5; i++) {
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+
+        if (playRound(humanChoice, computerChoice) === "") {
+            return "";
+        };
+
+        console.log(playRound(humanChoice, computerChoice));
+        console.log(showScore());
+    }
+
+    return humanScore > computerScore ? `YOU WON THE GAME` : `THE COMPUTER WON`
 }
 
 
-let humanScore = 0, computerScore = 0;
-let humanChoice = getHumanChoice()
-let computerChoice = getComputerChoice()
-console.log(playRound(humanChoice, computerChoice))
-console.log(humanScore, computerScore )
+console.log(playGame());
+
+
